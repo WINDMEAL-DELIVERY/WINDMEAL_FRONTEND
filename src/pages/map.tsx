3c11c3e1b02a6906b9061057b98e1554bfdcd55a
JoSooha <dlrhdcjs@naver.com>
2023-10-20 19:43:50 +0900

    feat: type(react-naver-map) install 및 클러스터에 숫자 표시 구현

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -11,44 +11,40 @@ import { useState } from 'react';
 import { makeMarkerClustering } from './marker-cluster';
 
 function MarkerCluster() {
-  // https://github.com/navermaps/marker-tools.js/blob/master/marker-clustering/src/MarkerClustering.js
-  // 예제에서 제공된 코드를 그대로 사용하되 naver 객체를 주입 받도록 간단히 makeMarkerClustering로 Wrapping 합니다.
-
   const navermaps = useNavermaps();
-  const map = useMap();
+  const map1 = useMap();
 
-  // https://github.com/zeakd/react-naver-maps/blob/main/website/src/samples/marker-cluster.js
   const MarkerClustering = makeMarkerClustering(window.naver);
 
   const htmlMarker1 = {
     content:
-      '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-1.png);background-size:contain;"></div>',
-    size: navermaps.Size(40, 40),
-    anchor: navermaps.Point(20, 20),
+      '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-1.png);background-size:contain;">a</div>',
+    size: new navermaps.Size(40, 40),
+    anchor: new navermaps.Point(20, 20),
   };
   const htmlMarker2 = {
     content:
       '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-2.png);background-size:contain;"></div>',
-    size: navermaps.Size(40, 40),
-    anchor: navermaps.Point(20, 20),
+    size: new navermaps.Size(40, 40),
+    anchor: new navermaps.Point(20, 20),
   };
   const htmlMarker3 = {
     content:
       '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-3.png);background-size:contain;"></div>',
-    size: navermaps.Size(40, 40),
-    anchor: navermaps.Point(20, 20),
+    size: new navermaps.Size(40, 40),
+    anchor: new navermaps.Point(20, 20),
   };
   const htmlMarker4 = {
     content:
       '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-4.png);background-size:contain;"></div>',
-    size: navermaps.Size(40, 40),
-    anchor: navermaps.Point(20, 20),
+    size: new navermaps.Size(40, 40),
+    anchor: new navermaps.Point(20, 20),
   };
   const htmlMarker5 = {
     content:
       '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-5.png);background-size:contain;"></div>',
-    size: navermaps.Size(40, 40),
-    anchor: navermaps.Point(20, 20),
+    size: new navermaps.Size(40, 40),
+    anchor: new navermaps.Point(20, 20),
   };
 
   // Customize Overlay 참고
@@ -58,19 +54,19 @@ function MarkerCluster() {
 
     const marker1 = new window.naver.maps.Marker({
       position: new window.naver.maps.LatLng(37.450795, 127.128816),
-      map,
+      map1,
     });
     const marker2 = new window.naver.maps.Marker({
       position: new window.naver.maps.LatLng(37.448, 127.1278),
-      map,
+      map1,
     });
     const marker3 = new window.naver.maps.Marker({
       position: new window.naver.maps.LatLng(37.447, 127.1282),
-      map,
+      map1,
     });
     const marker4 = new window.naver.maps.Marker({
       position: new window.naver.maps.LatLng(37.4487, 127.128),
-      map,
+      map1,
     });
 
     markers.push(marker1, marker2, marker3, marker4);
@@ -79,15 +75,14 @@ function MarkerCluster() {
     const cluster = new MarkerClustering({
       minClusterSize: 2,
       maxZoom: 17,
-      map,
+      map1,
       markers,
       disableClickZoom: false,
       gridSize: 120,
       icons: [htmlMarker1, htmlMarker2, htmlMarker3, htmlMarker4, htmlMarker5],
-      indexGenerator: [10, 100, 200, 500, 1000],
-      styling: function (clusterMarker: any, count: any) {
-        clusterMarker.getElement().querySelector('div:first-child').innerText =
-          count;
+      indexGenerator: [2, 4, 8, 12, 20],
+      stylingFunction: (clusterMarker: any, count: number) => {
+        clusterMarker.getElement().querySelector('div:first-child').innerText = count;
       },
     });
 
@@ -112,12 +107,11 @@ function MyMap() {
       }}
     >
       <MarkerCluster />
-      {/* <Marker position={new navermaps.LatLng(37.3595704, 127.105399)} /> */}
     </NaverMap>
   );
 }
 
-export default function map1() {
+export default function map() {
   return (
     <MapDiv
       style={{
