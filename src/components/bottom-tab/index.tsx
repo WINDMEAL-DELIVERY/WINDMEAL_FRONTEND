71922e9457bed70c947be8c2c371218b03d26897
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-11-02 20:14:29 +0900

    feat: 메인 경로 링크 수정

--- src/components/bottom-tab/index.tsx
+++ src/components/bottom-tab/index.tsx
@@ -4,7 +4,7 @@ export default function BottomTab() {
   return (
     <Wrapper>
       <NavItem href="/bulletin-board">게시글</NavItem>
-      <NavItem href="/">홈</NavItem>
+      <NavItem href="/main">홈</NavItem>
       <NavItem href="/chat-list">채팅</NavItem>
       <NavItem href="/my-page">마이페이지</NavItem>
     </Wrapper>
