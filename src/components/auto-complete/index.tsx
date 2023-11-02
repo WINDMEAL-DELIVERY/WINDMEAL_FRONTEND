be6150217f409fca195d74f892de0a6e50d2ab8f
JoSooha <dlrhdcjs@naver.com>
2023-11-03 01:13:58 +0900

    feat: auto-complete 자동완성 위한 searchHanler 구현

--- src/components/auto-complete/index.tsx
+++ src/components/auto-complete/index.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import { AutoComplete } from '@geist-ui/core';
 import { Wrapper } from './styles';
 
 export default function AutoCompleteBox() {
   // 경우에 따라 파라미터로 style이나 options를 받아 사용하도록 변경 예정
-  const options = [
-    { value: 'a', label: 'One' },
-    { value: 'b', label: 'Two' },
-    { value: 'c', label: 'Three' },
+  const allOptions = [
+    { value: '신의 한컵', label: '신의 한컵' },
+    { value: '컴포즈 커피', label: '컴포즈 커피' },
+    { value: '커피만', label: '커피만' },
   ];
+  const [options, setOptions] = useState<{ value: string; label: string }[]>(
+    [],
+  );
+  const searchHandler = (currentValue: string) => {
+    if (!currentValue) return setOptions([]);
+    const relatedOptions = allOptions.filter(item =>
+      item.value.includes(currentValue),
+    );
+    setOptions(relatedOptions);
+  };
 
   return (
     <Wrapper>
@@ -15,6 +26,7 @@ export default function AutoCompleteBox() {
         placeholder="Enter here"
         options={options}
         crossOrigin="anonymous"
+        onSearch={searchHandler}
         style={{
           zIndex: '999',
           backgroundColor: 'white',
