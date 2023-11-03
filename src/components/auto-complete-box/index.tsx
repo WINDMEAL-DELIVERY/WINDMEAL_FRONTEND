6dfe1d6b87efd3b38a0ef3c2779f13112c617f1f
JoSooha <dlrhdcjs@naver.com>
2023-11-03 13:08:28 +0900

    refactor: auto-complete 인터페이스 수정 및 types 예제 작성

--- src/components/auto-complete-box/index.tsx
+++ src/components/auto-complete-box/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { AutoComplete } from '@geist-ui/core';
 import { Wrapper } from '@components/auto-complete-box/styles';
+import { AutoCompleteOption } from '@geist-ui/core/esm/auto-complete';
 
 export default function AutoCompleteBox() {
   // 경우에 따라 파라미터로 style이나 options를 받아 사용하도록 변경 예정
@@ -9,9 +10,7 @@ export default function AutoCompleteBox() {
     { value: '컴포즈 커피', label: '컴포즈 커피' },
     { value: '커피만', label: '커피만' },
   ];
-  const [options, setOptions] = useState<{ value: string; label: string }[]>(
-    [],
-  );
+  const [options, setOptions] = useState<AutoCompleteOption[]>([]);
   const searchHandler = (currentValue: string) => {
     if (!currentValue) return setOptions([]);
     const relatedOptions = allOptions.filter(item =>
