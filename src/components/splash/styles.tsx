d3eee26d1e13501f3bf23fcc8ba8b8f1a65bf47c
jomo34 <trillion32@naver.com>
2023-11-03 13:39:57 +0900

    feat: 스플래시 스크린 구현 및 스타일 적용 초안

---
+++ src/components/splash/styles.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const ImagesDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const UponLogoImageDiv = styled.div`
+  width: 20%;
+  height: 20%;
+`;
+
+export const MainLogoImageDiv = styled.div`
+  width: 100%;
+  height: 100%;
+`;
