dfc8fa80d3ef9c2534a0f02f9bdd0a961c5d0684
JoSooha <dlrhdcjs@naver.com>
2023-11-24 23:42:45 +0900

    refactor: type, styled 분리

--- src/components/card-view/index.tsx
+++ src/components/card-view/index.tsx
@@ -1,114 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-// import tempImg from '../../../public/kakao.png';
-
-const CardContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  background-color: rgb(70, 70, 70);
-  justify-content: space-around;
-  align-items: center;
-  height: 25rem;
-`;
-
-const DeliveryContainer = styled.div`
-  margin-bottom: 1rem;
-  display: flex;
-  flex-direction: column;
-  width: 40%;
-  margin: 1.5rem;
-`;
-
-const DeliveryTitle = styled.h2`
-  color: white;
-  margin-bottom: 0.6rem;
-  margin-left: 1rem;
-  font-weight: bold;
-`;
-
-const NoDeliveryText = styled.h2`
-  font-weight: bold;
-`;
-
-const DeliveryCard = styled.div`
-  margin-bottom: 1rem;
-  background-color: rgb(200, 200, 200);
-  display: flex;
-  flex-direction: column;
-  height: 18rem;
-  border-radius: 2rem;
-  justify-content: center;
-  align-items: center;
-`;
-
-const DeliveryCardContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  height: 85%;
-  > * {
-    margin-bottom: 1rem; /* 자식 요소들 간에 하단 여백 */
-    &:last-child {
-      margin-bottom: 0; /* 마지막 자식에는 여백을 주지 않음 */
-    }
-  }
-`;
-
-const DeliveryMenu = styled.p``;
-
-const DeliveryStart = styled.p``;
-
-const DeliveryEnd = styled.p``;
-
-const DeliveryState = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const DeliveryCustomerWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
-
-const DeliveryCustomer = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-left: 0.4rem;
-`;
-
-const DeliveryCustomerImg = styled.img`
-  border-radius: 50%;
-  width: 3rem;
-  height: 3rem;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 2rem;
-  margin-right: auto;
-  margin-left: auto;
-  margin-top: auto;
-`;
-
-interface Menu {
-  menuName: string;
-}
-
-interface Delivery {
-  id: number;
-  menus: Menu[];
-  storeName: string;
-  destination: string;
-  customerName: string;
-  customerImg: string;
-  status: string;
-}
+import { Delivery } from '@/types/type';
+import {
+  CardContainer,
+  DeliveryContainer,
+  DeliveryTitle,
+  DeliveryCard,
+  DeliveryCardContent,
+  DeliveryMenu,
+  DeliveryStart,
+  DeliveryState,
+  DeliveryEnd,
+  DeliveryCustomerWrapper,
+  DeliveryCustomerImg,
+  DeliveryCustomer,
+  ButtonContainer,
+  NoDeliveryText,
+} from '@components/card-view/styles';
 
 export default function CardView() {
   const [deliveries, setDeliveries] = useState<Delivery[]>([
--- src/types/type.ts
+++ src/types/type.ts
@@ -1,5 +1,19 @@
-interface MyMapProps {
+export interface MyMapProps {
   selected: string | undefined;
   selectFlag: number;
   handleSelect: (selectedValue: string) => void;
+}
+
+interface Menu {
+  menuName: string;
+}
+
+export interface Delivery {
+  id: number;
+  menus: Menu[];
+  storeName: string;
+  destination: string;
+  customerName: string;
+  customerImg: string;
+  status: string;
 }
