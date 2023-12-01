bf5040b56b2ab94d9b41d6ba1b9244fdcae3e3a3
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-12-01 10:38:12 +0900

    refactor: bottom-tab tag boolean attribute 전달을 위한 transient props 사용

--- src/components/bottom-tab/index.tsx
+++ src/components/bottom-tab/index.tsx
@@ -7,17 +7,17 @@ export default function BottomTab() {
     <Wrapper>
       <NavItem
         href="/bulletin-board"
-        isActive={router.pathname === '/bulletin-board'}
+        $isActive={router.pathname === '/bulletin-board'}
       >
         게시글
       </NavItem>
-      <NavItem href="/main" isActive={router.pathname === '/main'}>
+      <NavItem href="/main" $isActive={router.pathname === '/main'}>
         홈
       </NavItem>
-      <NavItem href="/chat-list" isActive={router.pathname === '/chat-list'}>
+      <NavItem href="/chat-list" $isActive={router.pathname === '/chat-list'}>
         채팅
       </NavItem>
-      <NavItem href="/my-page" isActive={router.pathname === '/my-page'}>
+      <NavItem href="/my-page" $isActive={router.pathname === '/my-page'}>
         마이페이지
       </NavItem>
     </Wrapper>
