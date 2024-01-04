8e6cf99ac721d9c63acc0a87573a4b7754a88e37
jomo34 <trillion32@naver.com>
2024-01-04 20:27:48 +0900

    chore: 빌드 테스트

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -216,5 +216,5 @@ import { makeMarkerClustering } from '@/components/map/marker-cluster';
 import { MyMapProps, storeProp } from '@/types/type';
 
 export default function Map() {
-  return <div>gd</div>;
+  return <div>빌드 테스트</div>;
 }
