be66ad2babbaa973ca411f1275eb1814a012a737
JoSooha <dlrhdcjs@naver.com>
2024-01-12 23:59:12 +0900

    fix: window 객체 받아오는 코드 수정

--- src/components/bottom-sheet/BottomSheetOption.ts
+++ src/components/bottom-sheet/BottomSheetOption.ts
@@ -1,3 +1,5 @@
 export const MIN_Y = 60;
-export const MAX_Y = window.innerHeight - 160;
-export const BOTTOM_SHEET_HEIGHT = window.innerHeight - MIN_Y;
+export const MAX_Y =
+  typeof window !== 'undefined' ? window.innerHeight - 160 : 0;
+export const BOTTOM_SHEET_HEIGHT =
+  typeof window !== 'undefined' ? window.innerHeight - MIN_Y : 0;
