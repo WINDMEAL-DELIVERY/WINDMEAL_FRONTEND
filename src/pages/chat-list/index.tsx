85acc8da25a4860bb78be93be6933bbde437b5c6
jomo34 <trillion32@naver.com>
2024-01-22 01:32:45 +0900

    feat: 채팅방 리스트 불러오기 및 채팅방 출력 구현중

--- src/pages/chat-list/index.tsx
+++ src/pages/chat-list/index.tsx
@@ -23,27 +23,59 @@ import { useQuery } from 'react-query';
 import { getChattingList } from '@apis/chatting/chatting';
 import { useState } from 'react';
 import { ChattingListProps } from '@type/chattingType';
-import { useRouter } from 'next/router';
 
 export default function ChatList() {
-  const router = useRouter();
-
   const [chattingRooms, setChattingRooms] = useState<ChattingListProps[]>([]);
 
-  // useQuery<ChattingListProps[]>(
-  //   ['chattingList'],
-  //   async () => {
-  //     const { data } = await getChattingList();
-  //     return data.content;
-  //   },
-  //   {
-  //     onSuccess: chattingList => {
-  //       console.log(chattingList);
-  //       setChattingRooms(chattingList);
-  //     },
-  //     onError: err => console.log('!!', err),
-  //   },
-  // );
+  useQuery<ChattingListProps[]>(
+    ['chattingList'],
+    async () => {
+      const { data } = await getChattingList();
+      return data.chatroomSpecResponses.content;
+    },
+    {
+      onSuccess: chattingList => {
+        setChattingRooms(chattingList);
+      },
+      onError: err => console.log('!!', err),
+    },
+  );
+
+  function formatDateTime(dateTimeString: string) {
+    const inputDate = new Date(dateTimeString);
+    const currentDate = new Date();
+
+    if (
+      inputDate.getDate() === currentDate.getDate() &&
+      inputDate.getMonth() === currentDate.getMonth() &&
+      inputDate.getFullYear() === currentDate.getFullYear()
+    ) {
+      const hours = inputDate.getHours() % 12 || 12;
+      const minutes = inputDate.getMinutes();
+      const ampm = inputDate.getHours() >= 12 ? '오후' : '오전';
+      return `${ampm} ${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+    }
+
+    const yesterday = new Date(currentDate);
+    yesterday.setDate(currentDate.getDate() - 1);
+    if (
+      inputDate.getDate() === yesterday.getDate() &&
+      inputDate.getMonth() === yesterday.getMonth() &&
+      inputDate.getFullYear() === yesterday.getFullYear()
+    ) {
+      return '어제';
+    }
+
+    const year =
+      inputDate.getFullYear() !== currentDate.getFullYear()
+        ? `${inputDate.getFullYear()}.`
+        : '';
+    const month = inputDate.getMonth() + 1;
+    const day = inputDate.getDate();
+    return `${year}${month < 10 ? '0' : ''}${month}.${
+      day < 10 ? '0' : ''
+    }${day}`;
+  }
 
   return (
     <Wrapper>
@@ -54,60 +86,39 @@ export default function ChatList() {
         <Title>채팅</Title>
       </Header>
       <ChattingList>
-        <Chat href={`/chat-list/${1}`}>
-          <ProfileImageFrame>
-            <ProfileImage src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" />
-          </ProfileImageFrame>
-          <ChattingInfoFrame>
-            <ShopNDest>신의한컵, AI공학관</ShopNDest>
-            <NickNameNTime>
-              <NickName>닉네임1</NickName>
-              <Time>2분 전</Time>
-            </NickNameNTime>
-            <ChattingNUpdate>
-              <LastMessage>채팅 내용</LastMessage>
-              <Update number={2}>
-                <UpdateNums>2</UpdateNums>
-              </Update>
-            </ChattingNUpdate>
-          </ChattingInfoFrame>
-        </Chat>
-        <Chat>
-          <ProfileImageFrame>
-            <ProfileImage src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" />
-          </ProfileImageFrame>
-          <ChattingInfoFrame>
-            <ShopNDest>신의한컵, AI공학관</ShopNDest>
-            <NickNameNTime>
-              <NickName>닉네임1</NickName>
-              <Time>2분 전</Time>
-            </NickNameNTime>
-            <ChattingNUpdate>
-              <LastMessage>채팅 내용</LastMessage>
-              <Update number={20}>
-                <UpdateNums>20</UpdateNums>
-              </Update>
-            </ChattingNUpdate>
-          </ChattingInfoFrame>
-        </Chat>
-        <Chat>
-          <ProfileImageFrame>
-            <ProfileImage src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" />
-          </ProfileImageFrame>
-          <ChattingInfoFrame>
-            <ShopNDest>신의한컵, AI공학관</ShopNDest>
-            <NickNameNTime>
-              <NickName>닉네임1</NickName>
-              <Time>2분 전</Time>
-            </NickNameNTime>
-            <ChattingNUpdate>
-              <LastMessage>채팅 내용</LastMessage>
-              <Update number={100}>
-                <UpdateNums>100</UpdateNums>
-              </Update>
-            </ChattingNUpdate>
-          </ChattingInfoFrame>
-        </Chat>
+        {!chattingRooms ? (
+          <div>채팅방이 없습니다.</div>
+        ) : (
+          <>
+            {chattingRooms.map(chatRoom => (
+              <Chat
+                key={chatRoom.chatroomId}
+                href={`/chat-list/${chatRoom.chatroomId}`}
+              >
+                <ProfileImageFrame>
+                  <ProfileImage src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" />
+                </ProfileImageFrame>
+                <ChattingInfoFrame>
+                  <ShopNDest>신의한컵, AI공학관</ShopNDest>
+                  <NickNameNTime>
+                    <NickName>닉네임1</NickName>
+                    <Time>{formatDateTime(chatRoom.lastMessageTime)}</Time>
+                  </NickNameNTime>
+                  <ChattingNUpdate>
+                    <LastMessage>
+                      {chatRoom.messageType === 'TEXT'
+                        ? chatRoom.lastMessage.substring(0, 10)
+                        : '사진'}
+                    </LastMessage>
+                    <Update number={chatRoom.uncheckedMessageCount}>
+                      <UpdateNums>{chatRoom.uncheckedMessageCount}</UpdateNums>
+                    </Update>
+                  </ChattingNUpdate>
+                </ChattingInfoFrame>
+              </Chat>
+            ))}
+          </>
+        )}
       </ChattingList>
       <BottomTab />
     </Wrapper>
