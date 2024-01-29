8f88acbfdfe632c5eb5557589f150ee150fe1722
jomo34 <trillion32@naver.com>
2024-01-30 02:09:18 +0900

    feat: 상대방 메시지 디자인 변경 및 전달 시간 변경 함수 구현

--- src/pages/chat-list/[chatId]/index.tsx
+++ src/pages/chat-list/[chatId]/index.tsx
@@ -5,16 +5,22 @@ import {
   Alarm,
   Plus,
   Icons,
-  TimeStamp,
   ChattingHistory,
+  OpponentMessage,
+  MyMessage,
+  OpponentProfileImage,
+  OpponentTimeStamp,
+  OpponentNickName,
+  Message,
+  OpponentNicknameNMessageInfo,
 } from '@styles/chatIdStyles';
 import { BellOutlined, LeftOutlined, MoreOutlined } from '@ant-design/icons';
-import { Wrapper } from '@styles/styles';
 import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
 import { ChattingProps } from '@type/chattingType';
 import { getChatting } from '@apis/chatting/chatting';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Wrapper } from '@styles/styles';
 
 function ChatRoom() {
   const router = useRouter();
@@ -38,6 +44,17 @@ function ChatRoom() {
     },
   );
 
+  function formatDateTime(dateTimeString: string) {
+    const inputDate = new Date(dateTimeString);
+    const inputDateWithTimeZone = new Date(
+      inputDate.getTime() + 9 * 60 * 60 * 1000,
+    );
+    const hours = inputDateWithTimeZone.getHours() % 12 || 12;
+    const minutes = inputDateWithTimeZone.getMinutes();
+    const ampm = inputDateWithTimeZone.getHours() >= 12 ? 'PM' : 'AM';
+    return `${hours}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
+  }
+
   return (
     <Wrapper>
       <Header>
@@ -55,9 +72,32 @@ function ChatRoom() {
         </Icons>
       </Header>
       <ChattingHistory>
-        {chatroomId}
-        {orderId}
-        <TimeStamp>2024. 01. 07 일요일</TimeStamp>
+        {chatMessages ? (
+          chatMessages.map((message, index) => {
+            if (message.fromMe) {
+              return (
+                <MyMessage key={index}>
+                  <div>dd</div>
+                  {message.message}
+                </MyMessage>
+              );
+            }
+            return (
+              <OpponentMessage key={index}>
+                <OpponentProfileImage src="https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" />
+                <OpponentNicknameNMessageInfo>
+                  <OpponentNickName>{opponentNickname}</OpponentNickName>
+                  <Message>{message.message}</Message>
+                  <OpponentTimeStamp>
+                    {formatDateTime(message.sendTime)}
+                  </OpponentTimeStamp>
+                </OpponentNicknameNMessageInfo>
+              </OpponentMessage>
+            );
+          })
+        ) : (
+          <div />
+        )}
       </ChattingHistory>
     </Wrapper>
   );
--- src/styles/chatIdStyles.tsx
+++ src/styles/chatIdStyles.tsx
@@ -31,6 +31,56 @@ export const Alarm = styled.div``;
 
 export const Plus = styled.div``;
 
-export const ChattingHistory = styled.div``;
+export const ChattingHistory = styled.div`
+  width: 90%;
+  flex-direction: column;
+  display: flex;
+  padding: 1rem;
+  font: inherit;
+  text-decoration: none;
+  color: black;
+  cursor: default;
+`;
 
-export const TimeStamp = styled.div``;
+export const OpponentMessage = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  margin-bottom: 1rem;
+`;
+
+export const OpponentProfileImage = styled.div<{ src: string }>`
+  background-image: ${({ src }) => `url(${src})`};
+  background-size: cover;
+  background-position: center;
+  border-radius: 100%;
+  width: 40px;
+  height: 40px;
+  background-color: black;
+`;
+
+export const OpponentNicknameNMessageInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem;
+  gap: 0.3rem;
+`;
+
+export const OpponentNickName = styled.div`
+  font-weight: bold;
+`;
+
+export const Message = styled.div`
+  padding: 1rem;
+  background-color: #f2f4ff;
+  border-radius: 0 1rem 1rem 1rem;
+`;
+
+export const OpponentTimeStamp = styled.div`
+  color: #a8b1ce;
+  justify-content: flex-end;
+  display: flex;
+`;
+
+export const MyMessage = styled.div``;
