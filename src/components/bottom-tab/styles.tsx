4fd44b9beb0153dd9f8c72f2651903b4b94e360d
JoSooha <dlrhdcjs@naver.com>
2024-03-19 15:50:41 +0900

    feat: 바텀탭 Link화 및 prefetch 구현, style 수정 (height, color)

--- src/components/bottom-tab/styles.tsx
+++ src/components/bottom-tab/styles.tsx
@@ -1,39 +1,44 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+  align-items: center;
   position: fixed;
   bottom: 0;
-  padding: 2% 0 2% 0;
   background-color: whitesmoke;
   z-index: 3;
+  height: 56px;
 
   /* 핸드폰 */
   @media screen and (max-width: 768px) {
     width: 100%;
+    height: 56px;
   }
 
   /* 태블릿 */
   @media screen and (min-width: 769px) and (max-width: 1024px) {
     width: 50%;
+    height: 56px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   }
 
   /* 데스크탑 */
   @media screen and (min-width: 1025px) {
     width: 30%;
+    height: 56px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   }
 `;
 
-export const NavItem = styled.a<{ $isActive: boolean }>`
-  color: ${props => (props.$isActive ? 'green' : 'black')};
+export const NavItem = styled(Link)<{ $isActive: boolean }>`
+  color: ${props => (props.$isActive ? '#5776B9' : 'black')};
   text-decoration: none;
   cursor: pointer;
 
   &:hover {
-    color: green;
+    color: #5776b9;
   }
 `;
