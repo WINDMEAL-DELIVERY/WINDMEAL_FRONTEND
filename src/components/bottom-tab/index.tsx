212813e13096025e810b2a1f79759e0a54e4c6e1
JoSooha <dlrhdcjs@naver.com>
2024-03-24 15:50:48 +0900

    fix: auto-prefetch로 인한 prefetch 삭제

--- src/components/bottom-tab/index.tsx
+++ src/components/bottom-tab/index.tsx
@@ -5,28 +5,16 @@ export default function BottomTab() {
   const router = useRouter();
   return (
     <Wrapper>
-      <NavItem
-        href="/order-list"
-        prefetch
-        $isActive={router.pathname === '/order-list'}
-      >
+      <NavItem href="/order-list" $isActive={router.pathname === '/order-list'}>
         주문목록
       </NavItem>
-      <NavItem href="/main" prefetch $isActive={router.pathname === '/main'}>
+      <NavItem href="/main" $isActive={router.pathname === '/main'}>
         홈
       </NavItem>
-      <NavItem
-        href="/chat-list"
-        prefetch
-        $isActive={router.pathname === '/chat-list'}
-      >
+      <NavItem href="/chat-list" $isActive={router.pathname === '/chat-list'}>
         채팅
       </NavItem>
-      <NavItem
-        href="/my-page"
-        prefetch
-        $isActive={router.pathname === '/my-page'}
-      >
+      <NavItem href="/my-page" $isActive={router.pathname === '/my-page'}>
         마이페이지
       </NavItem>
     </Wrapper>
